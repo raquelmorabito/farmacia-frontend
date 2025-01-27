50bac1e32c9eb4b4e380b027fd295f3528ac3686
User <raquelmorabito@hotmail.com>
2025-01-27 11:45:33 -0300

    navibar, footer, home

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+﻿import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Farma&Cia</Link>
+
+                    <div className='flex gap-4'>
+                        Produto
+                        Categoria
+                        Cadastrar categoria
+                        Perfil
+                        Sair
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,38 @@
+
+function Home() {
+    return (
+        <>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Farma&Cia!
+                        </h2>
+                        <p className='text-xl'>
+                             A companhia certa para o seu bem-estar
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                                >
+                                Novo produto
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://i.imgur.com/OtxhuTh.png"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
+
